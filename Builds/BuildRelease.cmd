089325d80afd3fca09f0ef50d8c92ff6350aad10
jamie <jamiedavis68@gmail.com>
2014-04-07 19:59:54 +0100

    Restored the build command.

--- Builds/BuildRelease.cmd
+++ Builds/BuildRelease.cmd
@@ -1,8 +1,8 @@
-rem call "c:\Program Files (x86)\Microsoft Visual Studio 12.0\Common7\Tools\VSVARS32.bat"
+call "c:\Program Files (x86)\Microsoft Visual Studio 12.0\Common7\Tools\VSVARS32.bat"
 
-rem msbuild ..\ConsoleToolkit.sln /t:clean
-rem msbuild ..\ConsoleToolkit.sln /t:Rebuild /p:Configuration=Release
-rem copy ..\ConsoleToolKit\bin\release\*.dll "ConsoleToolkit 1.0.0.0"
+msbuild ..\ConsoleToolkit.sln /t:clean
+msbuild ..\ConsoleToolkit.sln /t:Rebuild /p:Configuration=Release
+copy ..\ConsoleToolKit\bin\release\*.dll "ConsoleToolkit 1.0.0.0"
 copy ..\ConsoleToolKit\bin\release\*.dll "ConsoleToolkit_current"
 git\git add .
 pause
