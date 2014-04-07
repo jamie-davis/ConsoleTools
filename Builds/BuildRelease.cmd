5a5549a6d8b056dcb59f236a743352ad68f041e4
jamie <jamiedavis68@gmail.com>
2014-04-07 19:59:10 +0100

    Added a folder to contain the current version to make it easier to script updating the library in other projects.

--- Builds/BuildRelease.cmd
+++ Builds/BuildRelease.cmd
@@ -1,7 +1,8 @@
-call "c:\Program Files (x86)\Microsoft Visual Studio 12.0\Common7\Tools\VSVARS32.bat"
+rem call "c:\Program Files (x86)\Microsoft Visual Studio 12.0\Common7\Tools\VSVARS32.bat"
 
-msbuild ..\ConsoleToolkit.sln /t:clean
-msbuild ..\ConsoleToolkit.sln /t:Rebuild /p:Configuration=Release
-copy ..\ConsoleToolKit\bin\release\*.dll "ConsoleToolkit 1.0.0.0"
-git\git add "ConsoleToolkit 1.0.0.0"
+rem msbuild ..\ConsoleToolkit.sln /t:clean
+rem msbuild ..\ConsoleToolkit.sln /t:Rebuild /p:Configuration=Release
+rem copy ..\ConsoleToolKit\bin\release\*.dll "ConsoleToolkit 1.0.0.0"
+copy ..\ConsoleToolKit\bin\release\*.dll "ConsoleToolkit_current"
+git\git add .
 pause
