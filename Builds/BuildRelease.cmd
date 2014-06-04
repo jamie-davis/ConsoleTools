a65d15568f6df25a896e0cc50d5d2ed16868bb0f
jamie <jamiedavis68@gmail.com>
2014-06-04 22:00:57 +0100

    Release 1.0.0.1

--- Builds/BuildRelease.cmd
+++ Builds/BuildRelease.cmd
@@ -2,7 +2,11 @@ call "c:\Program Files (x86)\Microsoft Visual Studio 12.0\Common7\Tools\VSVARS32
 
 msbuild ..\ConsoleToolkit.sln /t:clean
 msbuild ..\ConsoleToolkit.sln /t:Rebuild /p:Configuration=Release
-copy ..\ConsoleToolKit\bin\release\*.dll "ConsoleToolkit 1.0.0.0"
+
+if NOT EXIST "ConsoleToolkit %consoletoolsversion%" md "ConsoleToolkit %consoletoolsversion%"
+
+copy ..\ConsoleToolKit\bin\release\*.dll "ConsoleToolkit %consoletoolsversion%"
 copy ..\ConsoleToolKit\bin\release\*.dll "ConsoleToolkit_current"
+
 git\git add .
 pause
