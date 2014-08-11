32084f32d3e588ddc48fc6f57a9e61107fb36c54
jamie <jamiedavis68@gmail.com>
2014-08-11 18:04:10 +0100

    Added build with first cut of user input support.

--- ConsoleToolkit/Properties/AssemblyInfo.cs
+++ ConsoleToolkit/Properties/AssemblyInfo.cs
@@ -33,7 +33,7 @@ using System.Runtime.InteropServices;
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.3")]
+[assembly: AssemblyFileVersion("1.0.0.4")]
 [assembly: AssemblyInformationalVersion("ConsoleToolkit v1.0")]
 
 [assembly: InternalsVisibleTo("ConsoleToolkitTests")]
