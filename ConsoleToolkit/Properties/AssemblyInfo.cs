05b11daa16404d2b46528afd5370bf7933b6736a
jamie <jamiedavis68@gmail.com>
2014-08-19 21:26:49 +0100

    Added a new build

--- ConsoleToolkit/Properties/AssemblyInfo.cs
+++ ConsoleToolkit/Properties/AssemblyInfo.cs
@@ -33,7 +33,7 @@ using System.Runtime.InteropServices;
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.4")]
+[assembly: AssemblyFileVersion("1.0.0.5")]
 [assembly: AssemblyInformationalVersion("ConsoleToolkit v1.0")]
 
 [assembly: InternalsVisibleTo("ConsoleToolkitTests")]
