d6e81e940e7d0b40c7cce64ecdfe03d3ad515f9c
jamie <jamiedavis68@gmail.com>
2014-09-10 19:23:53 +0100

    Incremented the build version number.

--- ConsoleToolkit/Properties/AssemblyInfo.cs
+++ ConsoleToolkit/Properties/AssemblyInfo.cs
@@ -33,7 +33,7 @@ using System.Runtime.InteropServices;
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.7")]
+[assembly: AssemblyFileVersion("1.0.0.8")]
 [assembly: AssemblyInformationalVersion("ConsoleToolkit v1.0")]
 
 [assembly: InternalsVisibleTo("ConsoleToolkitTests")]
