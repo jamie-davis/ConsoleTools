0bb74ccaf8b60ed2ab6c0a50afd8f96f3b15fdc7
jamie <jamiedavis68@gmail.com>
2014-09-10 19:22:40 +0100

    Corrected previous change.
    It was a work in progress and should not have been committed.

--- ConsoleToolkitDemo/TableDataCommand.cs
+++ ConsoleToolkitDemo/TableDataCommand.cs
@@ -13,26 +13,8 @@ namespace ConsoleToolkitDemo
         public void Handle(TableDataCommand command, IConsoleOperations console)
         {
             var data = Enumerable.Range(0, 20)
-                .Select(i => new { Text = string.Format("item {0}", i), Index = i })
-                .Report();
+                .Select(i => new { Text = string.Format("item {0}", i), Index = i });
             console.FormatTable(data);
-        }
-
-    }
-
-    public class Report<T>
-    {
-        public Report(IEnumerable<T> items)
-        {
-            throw new System.NotImplementedException();
-        }
-    }
-
-    public static class ReportExtensions
-    {
-        public static Report<T> Report<T>(this IEnumerable<T> items)
-        {
-            return new Report<T>(items);
         }
 
     }
