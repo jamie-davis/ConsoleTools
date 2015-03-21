e3f56eccc7a26217b03657c89b8cada4b7eb6771
Jamie Davis <jamie@clandavis.net>
2015-03-21 11:12:38 +0000

    Added xml documentation to the build output.

--- Builds/BuildRelease.cmd
+++ Builds/BuildRelease.cmd
@@ -6,7 +6,12 @@ msbuild ..\ConsoleToolkit.sln /t:Rebuild /p:Configuration=Release
 if NOT EXIST "ConsoleToolkit %consoletoolsversion%" md "ConsoleToolkit %consoletoolsversion%"
 
 copy ..\ConsoleToolKit\bin\release\*.dll "ConsoleToolkit %consoletoolsversion%"
+copy ..\ConsoleToolKit\bin\release\*.xml "ConsoleToolkit %consoletoolsversion%"
 copy ..\ConsoleToolKit\bin\release\*.dll "ConsoleToolkit_current"
+copy ..\ConsoleToolKit\bin\release\*.xml "ConsoleToolkit_current"
 
 git\git add "ConsoleToolkit %consoletoolsversion%\*.dll" -f
+git\git add "ConsoleToolkit %consoletoolsversion%\*.xml" -f
+git\git add "ConsoleToolkit_current\*.dll" -f
+git\git add "ConsoleToolkit_current\*.xml" -f
 pause
