7265009b42ceb6cb831ba606f912c0366791ffd9
Jamie Davis <jamie@clandavis.net>
2015-04-20 08:14:57 +0100

    Added ReportAnalyser experimental utility.

---
+++ ConsoleToolkit/Utilities/ReportAnalyser.cs
@@ -0,0 +1,69 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using ConsoleToolkit.ConsoleIO;
+using ConsoleToolkit.ConsoleIO.Internal;
+
+namespace ConsoleToolkit.Utilities
+{
+    /// <summary>
+    /// A utility for analysing and extracting data from a <see cref="Report{T}"/>.
+    /// <para/>
+    /// Be aware that the report will be enumerated by this class and so it cannot also be formatted.
+    /// <remarks><para>Renderable column types are not supported. (For example, <see cref="RecordingConsoleAdapter"/>.)</para></remarks>
+    /// <remarks>Please note that this class is experimental and may change radically or be removed in a future version.</remarks>
+    /// </summary>
+    public class ReportAnalyser
+    {
+        /// <summary>
+        /// Construct an analyser for a report. The report will be enumerated by this class.
+        /// </summary>
+        /// <param name="report">The report to analyse.</param>
+        public static ReportAnalysis<T> Analyse<T>(Report<T> report)
+        {
+            return new ReportAnalysis<T>(report);
+        }
+    }
+
+    /// <summary>
+    /// The results of an analysis of a report.
+    /// <para/>
+    /// Be aware that the report will be enumerated by this class and so it cannot also be formatted.
+    /// </summary>
+    public class ReportAnalysis<T>
+    {
+        private readonly Report<T> _report;
+        private List<string> _columns;
+        private IEnumerator _enumerator;
+        private List<PropertyColumnFormat> _colFormats;
+
+        internal ReportAnalysis(Report<T> report)
+        {
+            _report = report;
+            _enumerator = _report.Query.GetEnumerator();
+            _colFormats = FormatAnalyser.Analyse(_report.RowType, _report.Columns, false);
+
+            _columns = report.Columns.Select(c => c.Heading).ToList();
+        }
+
+        public IEnumerable<string> Columns { get { return _columns; } }
+
+        public bool MoveNext()
+        {
+            return _enumerator.MoveNext();
+        }
+
+        public string GetColumnValue(string columnName)
+        {
+            var format = _colFormats.FirstOrDefault(f => f.Format.Heading == columnName);
+            if (format == null)
+                return string.Empty;
+
+            var value = format.Property.GetValue(_enumerator.Current);
+            if (value is IConsoleRenderer)
+                return string.Empty;
+
+            return ValueFormatter.Format(format.Format, value);
+        }
+    }
+}
---
+++ ConsoleToolkitTests/Utilities/TestReportAnalyser.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using ApprovalTests;
+using ApprovalTests.Reporters;
+using ConsoleToolkit.ConsoleIO;
+using ConsoleToolkit.ConsoleIO.Internal;
+using ConsoleToolkit.Testing;
+using ConsoleToolkit.Utilities;
+using ConsoleToolkitTests.TestingUtilities;
+using NUnit.Framework;
+
+namespace ConsoleToolkitTests.Utilities
+{
+    [TestFixture]
+    [UseReporter(typeof (CustomReporter))]
+    public class TestReportAnalyser
+    {
+        private Tuple<int, string>[] _data;
+        private Report<Tuple<int, string>> _report;
+        private UnitTestConsole _utc;
+
+        [SetUp]
+        public void SetUp()
+        {
+            _data = Enumerable.Range(0, 10)
+                .Select(x => Tuple.Create(x, string.Format("Num = {0}", x)))
+                .ToArray();
+            _report = _data.AsReport(rep => rep
+                .AddColumn(r => r.Item1, cc => cc.Heading("Number"))
+                .AddColumn(r => r.Item2, cc => cc.Heading("Text"))
+                );
+
+        _utc = new UnitTestConsole();
+            _utc.Interface.BufferWidth = 133;
+            _utc.Interface.WindowWidth = 133;
+        }
+
+        [Test]
+        public void ColumnsAreAvailable()
+        {
+            //Arrange
+            var analysis = ReportAnalyser.Analyse(_report);
+
+            //Act
+            var columns = string.Join(", ", analysis.Columns);
+
+            //Assert
+            Assert.That(columns, Is.EqualTo("Number, Text"));
+        }
+
+        [Test]
+        public void ColumnValuesFromEachRowAreAvailable()
+        {
+            //Arrange
+            var analysis = ReportAnalyser.Analyse(_report);
+
+            //Act
+            var rowData = GetAllRows(analysis).Select((s, n) => new {Row = n, Values = s});
+
+            //Assert
+            _utc.Console.FormatTable(rowData);
+            Approvals.Verify(_utc.Interface.GetBuffer());
+        }
+
+        private IEnumerable<string> GetAllRows<T>(ReportAnalysis<T> analysis)
+        {
+            while (analysis.MoveNext())
+            {
+                yield return string.Join(", ", analysis.Columns.Select(analysis.GetColumnValue));
+            }
+        }
+    }
+}
