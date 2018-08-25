7b064db152f3c887babb6268e1e7bcca5509c401
jamie-davis <jamie@clandavis.net>
2018-08-25 09:11:00 +0100

    Started kuldge to allow unit tests to run under .net standard. These changes may well be temporary, as the approval tests kludge will most likely be unable to support new tests in the way that ApprovalTests does without significant effort. The plan is to use the existing *.approved files to position the *.received file to enable the unit tests to be run at least once under ubuntu. At that point I will assess whether to extend the kludge to support the ConsoleToolkit's approval tests dependency. My initial assessment is that this will be too much effort.

---
+++ CoreConsoleTest/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using ConsoleToolkit;
+using ConsoleToolkit.ApplicationStyles;
+using ConsoleToolkit.CommandLineInterpretation.ConfigurationAttributes;
+using ConsoleToolkit.ConsoleIO;
+
+namespace CoreConsoleTest
+{
+    class Program : CommandDrivenApplication
+    {
+        static void Main(string[] args)
+        {
+            Toolkit.Execute<Program>(args);
+        }
+    }
+
+
+    [Command]
+    [Description("Display command line help")]
+    public class HelpCommand
+    {
+        [Positional(DefaultValue = null)]
+        [Description("The command on which help is required.")]
+        public List<string> Topic { get; set; }
+    }
+
+    [Command]
+    [Description("Display coloured text")]
+    public class TextCommand
+    {
+        [CommandHandler]
+        public void Handle(IConsoleAdapter console, IErrorAdapter error)
+        {
+            var coloured = "coloured".Red();
+            console.WrapLine($"Some nice {coloured} text!".Cyan());
+        }
+    }
+}
